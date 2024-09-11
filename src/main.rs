d1dd5707d91db4388b6cf8449f73d8799071281b
cauvmou <cauvmou.8855@gmail.com>
2024-09-11 15:25:53 +0200

    simplify services

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod storage;
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::string::FromUtf8Error;
-use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::sync::{LockResult, RwLock, RwLockReadGuard, RwLockWriteGuard};
 use clap::Parser;
 use log::{error, info, trace, warn};
 use crate::session::{Command, Session};
@@ -47,121 +47,62 @@ struct Args {
     cert: Option<std::path::PathBuf>,
 }
 
-// pub struct Application {
-//     pub sessions: HashMap<Uuid, Session>,
-//     pub secure: bool,
-//     pub port: usize,
-// }
-//
-// impl Default for Application {
-//     fn default() -> Self {
-//         Self { sessions: HashMap::new(), secure: false, port: 8080 }
-//     }
-// }
-
 #[actix_web::get("/")]
-async fn index(req: actix_web::HttpRequest, session_store: actix_web::web::Data<SessionStore>) -> impl actix_web::Responder {
+async fn index(req: actix_web::HttpRequest, session_store: actix_web::web::Data<RwLock<SessionStore>>) -> impl actix_web::Responder {
     if let (Some(header_user), Some(header_dir)) = (req.headers().get("x-User"), req.headers().get("x-Dir")) {
         if let (Ok(user), Ok(directory)) = (header_user.to_str().map(String::from), header_dir.to_str().map(String::from)) {
             let uuid = uuid::Uuid::new_v4();
-            info!("new client ({uuid}) with user: {user}");
-            let session = Session {
-                uuid: uuid.as_u128(),
-                last_seen: std::time::SystemTime::now(),
-                status: session::Status::Active,
-                metadata: session::SessionData {
-                    user,
-                    directory,
-                },
-            };
-            match (session_store.sessions.write().ok(), session_store.commands.write().ok(), session_store.session_lock.write().ok()) {
-                (Some(mut sessions), Some(mut commands), Some(mut lock)) => {
-                    sessions.insert(uuid.as_u128(), RwLock::new(session));
-                    commands.insert(uuid.as_u128(), RwLock::new(vec![]));
-                    lock.insert(uuid.as_u128(), RwLock::new(None));
+            return match session_store.write() {
+                Ok(mut store) => {
+                    info!("new client ({uuid}) with user: {user}");
+                    store.create_session(Session {
+                        uuid: uuid.as_u128(),
+                        last_seen: std::time::SystemTime::now(),
+                        status: session::Status::Active,
+                        metadata: session::SessionData {
+                            user,
+                            directory,
+                        },
+                    });
+                    actix_web::HttpResponse::Ok().body(uuid.to_string())
                 }
-                _ => {
-                    return actix_web::HttpResponse::InternalServerError().finish()
+                Err(_) => {
+                    actix_web::HttpResponse::InternalServerError().body("Failed to acquire session store lock!".to_string())
                 }
             }
-            return actix_web::HttpResponse::Ok().body(uuid.to_string());
         }
     }
     actix_web::HttpResponse::BadRequest().finish()
 }
 
 #[actix_web::get("/{uuid}")]
-async fn cmd_input(path: actix_web::web::Path<(String,)>, req: actix_web::HttpRequest, session_store: actix_web::web::Data<SessionStore>) -> impl actix_web::Responder {
+async fn cmd_input(path: actix_web::web::Path<(String,)>, req: actix_web::HttpRequest, session_store: actix_web::web::Data<RwLock<SessionStore>>) -> impl actix_web::Responder {
     if let (Some(header_user), Some(header_dir)) = (req.headers().get("x-User"), req.headers().get("x-Dir")) {
         if let (Ok(_), Ok(_)) = (header_user.to_str().map(String::from), header_dir.to_str().map(String::from)) {
             let Ok(uuid) = uuid::Uuid::parse_str(path.0.as_str()) else {
                 warn!("cannot parse uuid: {}", path.0.as_str());
                 return actix_web::HttpResponse::BadRequest().finish();
             };
-            let Some(commands) = session_store.session_lock.read().ok() else {
-                warn!("failed to acquire read lock for session_locks");
-                return actix_web::HttpResponse::InternalServerError().finish();
+            let Ok(store) = session_store.read() else {
+                return actix_web::HttpResponse::InternalServerError().body("Failed to acquire session store lock!".to_string())
             };
-            let Some(Some(pending_command)) = commands.get(&uuid.as_u128()).map(|e| e.read().ok()) else {
-                warn!("failed to acquire read lock for pending session command");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-            let command = pending_command.clone().unwrap_or("".to_string());
-            if pending_command.is_some() {
-                trace!("sending command {command:?} to host on session: {}", uuid.to_string());
+            return match store.get_pending_command(uuid.as_u128()) {
+                Ok(command) => actix_web::HttpResponse::Ok().body(command),
+                Err(err) => actix_web::HttpResponse::InternalServerError().body(err),
             }
-            return actix_web::HttpResponse::Ok().body(command);
         }
     }
     actix_web::HttpResponse::BadRequest().finish()
 }
 
 #[actix_web::post("/{uuid}")]
-async fn cmd_output(path: actix_web::web::Path<(String,), >, bytes: actix_web::web::Bytes, req: actix_web::HttpRequest, session_store: actix_web::web::Data<SessionStore>) -> impl actix_web::Responder {
+async fn cmd_output(path: actix_web::web::Path<(String,), >, bytes: actix_web::web::Bytes, req: actix_web::HttpRequest, session_store: actix_web::web::Data<RwLock<SessionStore>>) -> impl actix_web::Responder {
     if let (Some(header_user), Some(header_dir)) = (req.headers().get("x-User"), req.headers().get("x-Dir")) {
         if let (Ok(user), Ok(directory)) = (header_user.to_str().map(String::from), header_dir.to_str().map(String::from)) {
             let Ok(uuid) = uuid::Uuid::parse_str(path.0.as_str()) else {
                 warn!("cannot parse uuid: {}", path.0.as_str());
                 return actix_web::HttpResponse::BadRequest().finish();
             };
-            let Some(session_lock) = session_store.session_lock.read().ok() else {
-                warn!("failed to acquire read lock for session_locks");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-            let Some(pending_command) = session_lock.get(&uuid.as_u128()) else {
-                warn!("failed to get pending session command");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-            let Some(mut pending) = pending_command.write().ok() else {
-                error!("client sent an unexpected command output, no command was pending!");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-
-            let Some(input) = pending.clone() else {
-                error!("client sent an unexpected command output, no command was pending!");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-
-            *pending = None;
-
-            let Some(sessions) = session_store.sessions.read().ok() else {
-                warn!("failed to acquire read lock for session_locks");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-
-            let Some(session) = sessions.get(&uuid.as_u128()) else {
-                warn!("failed to get session");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-
-            let Some(mut session) = session.write().ok() else {
-                warn!("failed to acquire write lock for session");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-
-            session.metadata.user = user.to_owned();
-            session.metadata.directory = directory.to_owned();
-
             let output = match String::from_utf8(bytes.to_vec()) {
                 Ok(string) => string,
                 Err(err) => {
@@ -169,30 +110,17 @@ async fn cmd_output(path: actix_web::web::Path<(String,), >, bytes: actix_web::w
                     return actix_web::HttpResponse::BadRequest().finish();
                 }
             };
-
-            let command = Command {
-                timestamp: std::time::SystemTime::now(),
-                input,
-                output,
+            let Ok(store) = session_store.read() else {
+                return actix_web::HttpResponse::InternalServerError().body("Failed to acquire session store lock!".to_string())
             };
-
-            let Some(commands) = session_store.commands.read().ok() else {
-                warn!("failed to acquire read lock for commands");
-                return actix_web::HttpResponse::InternalServerError().finish();
+            let command = match store.resolve_command(uuid.as_u128(), output) {
+                Ok(command) => command,
+                Err(err) => return actix_web::HttpResponse::InternalServerError().body(format!("{err}").to_string())
             };
-
-            let Some(commands) = commands.get(&uuid.as_u128()) else {
-                warn!("failed to get for commands");
-                return actix_web::HttpResponse::InternalServerError().finish();
+            return match store.insert_command(uuid.as_u128(), command) {
+                Ok(_) => actix_web::HttpResponse::Ok().finish(),
+                Err(err) => actix_web::HttpResponse::InternalServerError().body(format!("{err}").to_string())
             };
-
-            let Some(mut commands) = commands.write().ok() else {
-                warn!("failed to acquire write lock for commands");
-                return actix_web::HttpResponse::InternalServerError().finish();
-            };
-            commands.push(command);
-
-            return actix_web::HttpResponse::Ok().finish();
         }
     }
     actix_web::HttpResponse::BadRequest().finish()
@@ -209,7 +137,7 @@ async fn main() -> std::io::Result<()> {
 
     let mut server = actix_web::HttpServer::new(|| {
         actix_web::App::new()
-            .app_data(actix_web::web::Data::new(SessionStore::default()))
+            .app_data(actix_web::web::Data::new(RwLock::new(SessionStore::default())))
             .service(index)
             .service(cmd_input)
             .service(cmd_output)
--- src/storage.rs
+++ src/storage.rs
@@ -1,12 +1,64 @@
 use std::cell::Cell;
 use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, LockResult, RwLock};
 use serde::{Deserialize, Serialize};
 use crate::session::{Command, Session};
 
 #[derive(Debug, Deserialize, Serialize, Default)]
 pub struct SessionStore {
-    pub sessions: RwLock<HashMap<u128, RwLock<Session>>>,
-    pub session_lock: RwLock<HashMap<u128, RwLock<Option<String>>>>,
-    pub commands: RwLock<HashMap<u128, RwLock<Vec<Command>>>>,
+    pub sessions: HashMap<u128, RwLock<Session>>,
+    pub session_lock: HashMap<u128, RwLock<Option<String>>>,
+    pub commands: HashMap<u128, RwLock<Vec<Command>>>,
+}
+
+impl SessionStore {
+    pub fn create_session(&mut self, session: Session) {
+        let uuid = session.uuid;
+        self.sessions.insert(uuid, RwLock::new(session));
+        self.session_lock.insert(uuid, RwLock::new(None));
+        self.commands.insert(uuid, RwLock::new(Vec::new()));
+    }
+
+    pub fn start_command(&self, uuid: u128, input: String) -> Result<(), String> {
+        let current = self.session_lock.get(&uuid).ok_or("No session for uuid!".to_string())?;
+        let mut current = current.write().ok().ok_or("Cannot acquire session lock!".to_string())?;
+        if let Some(command) = current.take() {
+            Err(format!("A command is already pending: {command:?}!").to_string())
+        } else {
+            *current = Some(input);
+            Ok(())
+        }
+    }
+    
+    pub fn get_pending_command(&self, uuid: u128) -> Result<String, String> {
+        let current = self.session_lock.get(&uuid).ok_or("No session for uuid!".to_string())?;
+        let mut current = current.write().ok().ok_or("Cannot acquire session lock!".to_string())?;
+        let command = current.take().ok_or("Already empty!".to_string())?;
+        Ok(command)
+    }
+    
+    fn clear_pending_command(&self, uuid: u128) -> Result<String, String> {
+        let current = self.session_lock.get(&uuid).ok_or("No session for uuid!".to_string())?;
+        let mut current = current.write().ok().ok_or("Cannot acquire session lock!".to_string())?;
+        let command = current.take().ok_or("Already empty!".to_string())?;
+        *current = None;
+        Ok(command)
+    }
+
+    pub fn resolve_command(&self, uuid: u128, output: String) -> Result<Command, String> {
+        let input = self.clear_pending_command(uuid)?;
+        let command = Command {
+            timestamp: std::time::SystemTime::now(),
+            input,
+            output,
+        };
+        Ok(command)
+    }
+
+    pub fn insert_command(&self, uuid: u128, command: Command) -> Result<(), String> {
+        let commands = self.commands.get(&uuid).ok_or("No session for uuid!".to_string())?;
+        let mut commands = commands.write().ok().ok_or("Cannot acquire commands lock!".to_string())?;
+        commands.push(command);
+        Ok(())
+    }
 }
